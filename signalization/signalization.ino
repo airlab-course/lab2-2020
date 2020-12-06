a4203a8e7c1fca11d9905cb16111cd7bb33d1a43
Axonny <KawaiiAxonny@gmail.com>
2020-12-06 12:38:00 +0500

    add pinout

--- signalization/signalization.ino
+++ signalization/signalization.ino
@@ -1,6 +1,10 @@
-#define PIN_SENSOR 0
-#define PIN_BUTTON_OFF 0
-#define PIN_BUZZER 0
+#include "pitches.h"
+#include "button.h"
+#include "buzzer.h"
+
+#define PIN_SENSOR A0
+#define PIN_BUTTON_OFF 22
+#define PIN_BUZZER 24
 
 Buzzer buzzer(PIN_BUZZER);
 Button buttonOff(PIN_BUTTON_OFF);
@@ -9,14 +13,17 @@ int notes[] = {NOTE_A4, NOTE_SILENCE};
 double durations[] = {8, 4};
 int melodyLength = 2;
 
-void setup() {
+void setup() 
+{
   pinMode(PIN_SENSOR, INPUT);
   buzzer.setMelody(notes, durations, melodyLength);
 }
 
-void loop() {
+void loop() 
+{
   double dist = readDistFilter(PIN_SENSOR);
-  if(dist > 10 && dist < 80){
+  if(dist > 10 && dist < 80) // range of correct work
+  {
     buzzer.turnSoundOn();
   }
 
@@ -26,11 +33,13 @@ void loop() {
   }
 }
 
-double readDistFilter(int pin){
+double readDistFilter(int pin)
+{
   int iterationsCount = 12;
   double sum = 0;
   double minimal = 1e10, maximal = -1;
-  for(int i = 0; i < iterationsCount; i++){  
+  for(int i = 0; i < iterationsCount; i++) 
+  {  
     double tmp = readDist(pin);
     sum += tmp;
     minimal = min(minimal, tmp);
@@ -41,11 +50,13 @@ double readDistFilter(int pin){
   return sum / (iterationsCount - 2);
 }
 
-double readDist(int pin){
+double readDist(int pin)
+{
   double volts = readVoltage(pin);
   return pow(14.7737/volts, 1.2134);
 }
 
-double readVoltage(int pin){
+double readVoltage(int pin)
+{
   return analogRead(pin) * 5.0 / 1024.0;
 }
