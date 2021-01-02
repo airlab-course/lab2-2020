d3f59dd2abe2f440ae11790f484b77fa305d6c00
shemloft <shem.dar99@gmail.com>
2021-01-02 18:00:27 +0500

    задачка решена

--- lab2/lab2.ino
+++ lab2/lab2.ino
@@ -1,9 +1,46 @@
+#include <Servo.h>
+
+const int echoPin = 8;
+const int trigPin = 9;
+
+Servo speedometerServo;
+
+const float delayBetweenMeasuresInMs = 10.0;
+
 void setup() {
-  // put your setup code here, to run once:
+  speedometerServo.attach(10);
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT);
+  digitalWrite(trigPin, LOW);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  float firstDist = readDist();
+  delay(delayBetweenMeasuresInMs);
+  float secondDist = readDist();
 
+  float speedInCmPerMs = abs(secondDist - firstDist) / delayBetweenMeasuresInMs;
+  float speedInKmPerHour = (speedInCmPerMs / 100000.0) * 3600000.0;
+
+  int angleForServo = (int)constrain(speedInKmPerHour, 0, 180);
+  speedometerServo.write(angleForServo);
+}
+
+float readDist()
+{
+  const float speedOfSoundMPerSec = 340.0;
+  const float speedOfSoundCmPerMicrosecond = speedOfSoundMPerSec / 10000.0;
+  return readPulse() * speedOfSoundCmPerMicrosecond / 2.0;
+}
+
+float readPulse()
+{
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH);
+
+  return duration;
 }
